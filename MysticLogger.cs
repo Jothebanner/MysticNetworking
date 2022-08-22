70575903132810d2a61b8adcbb2eb80398f1c67c
Jothebanner <hawks.jacob.g@gmail.com>
2022-08-22 15:19:36 -0600

    Removed unneeded namespaces. Flushed out the warning and exception loggers. Added summary documentation for the less-obvious members.

--- MysticLogger.cs
+++ MysticLogger.cs
@@ -1,38 +1,97 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using System;
 
 public class MysticLogger
 {
-	public delegate void CustomLogger(string log);
-	public delegate void CustomExceptionLogger(Exception exception);
+	private delegate void CustomLogger(string log);
+    private delegate void CustomWarningLogger(string log);
+	private delegate void CustomExceptionLogger(Exception exception);
 
 	static CustomLogger logger;
+	static CustomWarningLogger wLogger;
 	static CustomExceptionLogger eLogger;
+
+    /** <summary>   
+         Custom all-purpose logger.
+     <param name="_logger"/> A function which takes a string as a parameter.
+     <example>
+         <para> Example:  <c>MysticLogger.SetLoggingFunction((string text) => Console.WriteLine(text));</c> </para>
+     </example>
+     </summary> */
 	public static void SetLoggingFunction(CustomLogger _logger)
 	{
 		logger = _logger;
+        SetLoggingFunction((string text) => Console.WriteLine(text));
+	}
+    
+    /** <summary>   
+         Custom logger intended for logging warnings.
+     <param name="_logger"/> Any function which takes a string as a parameter.
+     <example>
+         <c>MysticLogger.SetLoggingFunction((string text) => Console.WriteLine(text));</c>
+     </example>
+     </summary> */
+	public static void SetWarningLoggingFunction(CustomWarningLogger _logger)
+	{
+		wLogger = _logger;
 	}
 	
+    /** <summary>   
+         Custom exception logger. Hook up to a string output function to receive logs from MysticNetworking.
+     <param name="_logger"/> Any function which takes a string as a parameter.
+     <example>
+         <c>MysticLogger.SetLoggingFunction((Execption e) => Console.WriteLine(e));</c>
+     </example>
+     </summary> */
 	public static void SetExceptionLoggingFunction(CustomExceptionLogger _logger)
 	{
 		eLogger = _logger;
 	}
 
-	public static void Log(string message)
+    /** <summary>   
+         Set this function for all loggers.
+     <param name="_logger"/> Any function which takes a string as a parameter.
+     <example>
+         <c>MysticLogger.SetAllLoggingFunctions((string text) => Console.WriteLine(text));</c>
+     </example>
+     </summary> */
+    public static void SetAllLoggingFunctions(CustomLogger _logger)
+    {
+        logger = _logger;
+        wLogger = _logger;
+        eLogger = _logger;
+    }
+
+	public static void Log(object message)
 	{
-		logger(message);
-	}
-	
-	public static void Log(int message)
-	{
-		logger(message.ToString());
+        string messageString = object.ToString();
+		logger(messageString);
 	}
 
-	public static void Log(Exception exception)
+	public static void LogWarning(object message)
 	{
-		eLogger(exception);
+        string messageString = object.ToString();
+		wLogger(messageString);
+	}
+
+	public static void LogException(Exception exception)
+	{
+        // if the exception logger is not set then fallback to the other loggers.
+        // unfortunatly we cannot log this so we gotta throw an exeption. Perhaps this is dumb idk
+
+        try{
+            // display as an exeption if possible
+            if (eLogger == CustomExceptionLogger)
+                eLogger(exception);
+            // fallbacks
+            else if (wLogger == CustomWarningLogger)
+                wLogger(exception);
+            else
+                logger(exception);
+        }
+        catch(Exception ex)
+        {
+            throw new NullReferenceException("No logging function has been set for MysticLogger", ex);
+        }
 	}
 
 }
